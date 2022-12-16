0df085567dc96511296d58769d25fa61e0887998
Tripp Hill <thillmfg@gmail.com>
2022-12-16 15:36:57 -0500

    fix build

--- Userland/Games/AdventOfCode2022/day5.cpp
+++ Userland/Games/AdventOfCode2022/day5.cpp
@@ -1,16 +1,21 @@
 //
 // Created by TrippHill on 12/16/2022.
 //
-#include "../../../AK/Stack.h"
+
 #include "AK/Stack.h"
 #include "AK/Vector.h"
 class Day5 {
+public:
+void test() {
+
+}
 private:
-    struct CrateStack {
-        AK::Stack<Crate>
-    };
+
     struct Crate {
         char letter;
     };
-};
+    struct CrateStack {
+        AK::Vector<AK::Stack<Crate,100>> stacks{};
+    };
 
+};
