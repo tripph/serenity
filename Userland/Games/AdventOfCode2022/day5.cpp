a7e73e95284be00c038d3930729194d932cbd547
Tripp Hill <thillmfg@gmail.com>
2022-12-16 14:28:13 -0500

    adding advent of code game

---
+++ Userland/Games/AdventOfCode2022/day5.cpp
@@ -0,0 +1,16 @@
+//
+// Created by TrippHill on 12/16/2022.
+//
+#include "../../../AK/Stack.h"
+#include "AK/Stack.h"
+#include "AK/Vector.h"
+class Day5 {
+private:
+    struct CrateStack {
+        AK::Stack<Crate>
+    };
+    struct Crate {
+        char letter;
+    };
+};
+
---
+++ Userland/Games/AdventOfCode2022/main.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/URL.h>
+#include <LibConfig/Client.h>
+#include <LibCore/System.h>
+#include <LibDesktop/Launcher.h>
+#include <LibGUI/Action.h>
+#include <LibGUI/Application.h>
+#include <LibGUI/BoxLayout.h>
+#include <LibGUI/Button.h>
+#include <LibGUI/Icon.h>
+#include <LibGUI/Menu.h>
+#include <LibGUI/Menubar.h>
+#include <LibGUI/Window.h>
+#include <LibMain/Main.h>
+#include <stdio.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
+{
+    TRY(Core::System::pledge("stdio rpath recvfd sendfd unix"));
+
+    auto app = TRY(GUI::Application::try_create(arguments));
+
+    Config::pledge_domain("AdventOfCode2022");
+
+    TRY(Desktop::Launcher::seal_allowlist());
+
+    TRY(Core::System::pledge("stdio rpath recvfd sendfd"));
+
+    TRY(Core::System::unveil("/tmp/session/%sid/portal/launch", "rw"));
+    TRY(Core::System::unveil("/res", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
+
+    auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-advent-of-code-2022"sv));
+
+    auto window = TRY(GUI::Window::try_create());
+
+    window->set_double_buffering_enabled(false);
+    window->set_title("Advent of Code 2022");
+    window->resize(324, 344);
+
+    window->show();
+
+    window->set_icon(app_icon.bitmap_for_size(16));
+
+    return app->exec();
+}
